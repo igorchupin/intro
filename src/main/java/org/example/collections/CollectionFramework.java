650ed954b94e5509cd04d016dadcf8d801d16bb3
tt7649362 <tt7649362@gmail.com>
2023-02-03 21:39:49 +0300

    current update

--- src/main/java/org/example/collections/CollectionFramework.java
+++ src/main/java/org/example/collections/CollectionFramework.java
@@ -1,54 +1,150 @@
 package org.example.collections;
 
 import java.util.*;
+import java.util.function.Consumer;
 
 public class CollectionFramework {
 
     public static void main(String[] args) {
         Collection<Dog> dogCollection = new ArrayList<>();
-
+//
+//        Dog[] dogs = new Dog[3];
+//        dogs[0] = new Dog("Bobik");
+//        dogs[1] = new Dog("Bobik");
+//        dogs[2] = new Dog("Bobik");
+//        dogs[3] = new Dog("Bobik");
+//
         Dog dog = new Dog("Bobik");
         Dog dog1 = new Dog("Tyzik");
-        dogCollection.add(dog);
-        dogCollection.add(dog);
-        System.out.println(dogCollection.size());
-        dogCollection.remove(dog);
-        System.out.println(dogCollection.size());
-        System.out.println(dogCollection.contains(new Dog("Sharik")));
+//        dogCollection.add(dog);
+//        dogCollection.add(dog);
+//        System.out.println(dogCollection.size());
+//        dogCollection.remove(dog);
+//        System.out.println(dogCollection.size());
+//        System.out.println(dogCollection.contains(new Dog("Sharik")));
+//
+//
+//
+//        System.out.println("************");
+//        List<Dog> dogList = new ArrayList<>();
+//        dogList.add(dog);
+//        dogList.add(dog1);
+//        dogList.get(0);
+//
+//        System.out.println("************");
+//        Set<Dog> dogSet = new HashSet<>();
+//        dogSet.add(dog);
+//        System.out.println(dogSet.add(dog));
+//        System.out.println(dogSet.size());
+//
+//
+//        System.out.println("************");
+//        Queue<Dog> dogQueue = new ArrayDeque<>();
+//        dogQueue.add(dog);
+//        dogQueue.add(dog1);
 
-
-
-        System.out.println("************");
-        List<Dog> dogList = new ArrayList<>();
-        dogList.add(dog);
-        dogList.add(dog1);
-        dogList.get(0);
-
-
-        System.out.println("************");
-        Set<Dog> dogSet = new HashSet<>();
-        dogSet.add(dog);
-        System.out.println(dogSet.add(dog));
-        System.out.println(dogSet.size());
-
-
-        System.out.println("************");
-        Queue<Dog> dogQueue = new ArrayDeque<>();
-        dogQueue.add(dog);
-        dogQueue.add(dog1);
-
-        System.out.println(dogQueue.poll().name);
-        System.out.println(dogQueue.poll().name);
+//        System.out.println(dogQueue.poll().name);
+//        System.out.println(dogQueue.poll().name);
 //        System.out.println(dogQueue.element().name);
+//
+//        Deque<Dog> dogDeque = new ArrayDeque<>();
+//        dogDeque.add(dog);
+//        dogDeque.add(dog1);
+//
+//        System.out.println(dogDeque.pollLast().name);
+//        System.out.println(dogDeque.getLast().name);
 
-        Deque<Dog> dogDeque = new ArrayDeque<>();
-        dogDeque.add(dog);
-        dogDeque.add(dog1);
 
-        System.out.println(dogDeque.pollLast().name);
-        System.out.println(dogDeque.getLast().name);
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("");
+        strings.add("");
+        strings.add(0,":");
+        System.out.println(strings.size());
+        strings.trimToSize();
+        System.out.println(strings.size());
+        strings.add(1, "");
+
+        LinkedList<String> stringLinkedList= new LinkedList<>();
+
+        stringLinkedList.add(0, "0");
+        stringLinkedList.add(1, "1");
+        stringLinkedList.add(2, "2");
+        stringLinkedList.add(1, "3");
+        System.out.println(stringLinkedList);
+        System.out.println(stringLinkedList);
 
 
+        HashSet<Dog> stringHashSet = new HashSet<>();
 
+        stringHashSet.add(dog1);
+        stringHashSet.add(dog);
+
+
+        System.out.println(dog1);
+
+        TreeSet<Integer> integerTreeSet = new TreeSet<>();
+
+        integerTreeSet.add(2);
+        integerTreeSet.add(1);
+        integerTreeSet.add(3);
+
+        Comparator<Dog> comparator = new Comparator<Dog>() {
+            @Override
+            public int compare(Dog o1, Dog o2) {
+                return 1;
+            }
+        };
+
+
+        TreeSet<Dog> dogTreeSet = new TreeSet<>(comparator);
+        dogTreeSet.add(dog1);
+        dogTreeSet.add(dog);
+        dogTreeSet.add(new Dog("Barbos"));
+
+        System.out.println(dogTreeSet);
+
+//        Iterator<Dog> iterator = dogTreeSet.iterator();
+
+//        while (iterator.hasNext()) {
+//            iterator.next();
+//            iterator.remove();
+//            System.out.println(dogTreeSet.size());
+//        }
+//
+//        iterator = dogTreeSet.iterator();
+//
+//        iterator.next();
+//        iterator.next();
+//
+//        Consumer<Dog> dogConsumer = new Consumer<Dog>() {
+//            @Override
+//            public void accept(Dog dog) {
+//                System.out.println(dog.getName());
+//            }
+//        };
+//
+//        iterator.forEachRemaining(dogConsumer);
+
+        for (Dog dogFromCollection : dogTreeSet) {
+            System.out.println(dogFromCollection.getName());
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("One", 1);
+        map.put("Two", 2);
+        map.put("Three", 3);
+        System.out.println(map);
+
+        map.forEach((k, v) -> {
+
+        });
+
+        Set<String> strings1 = map.keySet();
+        System.out.println(strings1);
+
+        Set<Map.Entry<String, Integer>> entries = map.entrySet();
+        entries.forEach(node -> {
+            System.out.println(node.getKey());
+        });
     }
 }
--- src/main/java/org/example/collections/Dog.java
+++ src/main/java/org/example/collections/Dog.java
@@ -1,10 +1,56 @@
 package org.example.collections;
 
-public class Dog {
+import java.util.Objects;
 
-    public String name;
+public class Dog implements Comparable<Dog> {
+
+    private String name;
+    private int age = 1;
 
     public Dog(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return age == dog.age && Objects.equals(name, dog.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+
+    @Override
+    public int compareTo(Dog o) {
+        return -1;
     }
 }
